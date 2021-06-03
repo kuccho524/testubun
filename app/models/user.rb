4e87d0552d3a34d44b316187969846ac3535bd11
kuccho524 <kucchachachanocha@gmail.com>
2021-06-03 03:05:24 +0000

    Create relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,30 @@ class User < ApplicationRecord
   has_many :train_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # 自分がフォローされる側の関係性
+  has_many :passive_of_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+  # 自分がフォローする側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  # 自分をフォローしている人
+  has_many :followers, through: :passive_of_relationships, source: :follower
+  # 自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
+  # フォローする
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  # フォロー外す
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしているか
+  def following?(user)
+    followings.include?(user)
+  end
+
   # バリデーション
   validates :name, presence: true
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'searches/search' => "searches#search"
-  resources :users, only: [:show, :edit, :update]
+  devise_for :users
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'follows'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :trains do
     resource :favorites, only: [:create, :destroy]
     resources :train_comments, only: [:create, :destroy]
   end
-  devise_for :users
 end
